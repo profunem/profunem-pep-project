e5acad728643b4ac417d10f25c01ef82a7b33e1b
Kung Lo <professionally_unemployed@proton.me>
2023-05-09 00:15:22 +0000

    Saving

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 import Model.Message;
 import Util.ConnectionUtil;
@@ -35,6 +36,28 @@ public class MessageDAO {
         }
         
         return null;
+    }
+
+    public List<Message> getAllMessage()
+    {
+        Connection connection = ConnectionUtil.getConnection();
+        List<Message> messages = new ArrayList<>();
+
+        try {
+            //Write SQL logic here
+            String sql = "SELECT * FROM message;";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                Message message = new Message(rs.getInt("message_id"), rs.getInt("posted_by"), rs.getString("message_text"), rs.getLong("time_posted_epoch"));
+                messages.add(message);
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        
+        return messages;
     }
     
 }
